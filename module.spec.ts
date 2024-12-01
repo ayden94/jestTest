dbde2e8c205dfcf5c0bd61b8ef18e8fccc4d4580
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-12-01 20:58:16 +0900

    a

--- module.spec.ts
+++ module.spec.ts
@@ -1,12 +1,8 @@
-jest.mock('./module', () => ({
-  obj: {
-    method: jest.fn().mockReturnValue("method1"),
-    props: "a"
-  }
-}));
 import { obj } from './module';
+jest.mock('./module');
 
 test('module', () => {
+  console.dir(obj.method)
   expect(obj.method()).toBe("method1");
   expect(obj.props).toBe("a");
 })
